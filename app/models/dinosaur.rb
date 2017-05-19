b2dd18a908625e343a97fd0d559e42787389fdb4
jesseborm <jesseborm@gmail.com>
2017-05-19 11:56:25 +0200

    Add model methods and seed file

--- app/models/dinosaur.rb
+++ app/models/dinosaur.rb
@@ -8,4 +8,5 @@ class Dinosaur < ApplicationRecord
     age < 3
   end
 
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,10 @@
+def rand_age
+  1 + rand(100)
+end
+
+
 dino1 = Dinosaur.create!( { name: "Iguanodon", age: 3, image_url: "https://goo.gl/wB6q9q" } )
 dino2 = Dinosaur.create!( { name: "Mosasaurus", age: 10, image_url: "https://goo.gl/aoZsYt" } )
 dino3 = Dinosaur.create!( { name: "Hungarosaurus", age: 15, image_url: "https://goo.gl/EqRlgP" } )
+
+dino4 = Dinosaur.create!({ name: "Giraffatitan brancai", age: rand_age, image_url: "https://en.wikipedia.org/wiki/Dinosaur#/media/File:Giraffatitan_scale.png" })
