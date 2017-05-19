799945bb900487e01df935748937546b3ec0dbba
jesseborm <jesseborm@gmail.com>
2017-05-19 15:00:17 +0200

    Create show and new dino pages

--- app/controllers/dinosaurs_controller.rb
+++ app/controllers/dinosaurs_controller.rb
@@ -4,4 +4,35 @@ class DinosaursController < ApplicationController
     @dinosaurs = Dinosaur.all
   end
 
+  def show
+    @dinosaur = Dinosaur.find(params[:id])
+  end
+
+  def new
+    @dinosaur = Dinosaur.new
+  end
+
+  def create
+    # debugger
+    @dinosaur = Dinosaur.new(dinosaur_params)
+
+    if @dinosaur.save
+      redirect_to @dinosaur
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @dinosaur = Dinosaur.update #FIXME
+  end
+
+  def destroy
+    @dinosaur = Dinosaur.delete #FIXME
+  end
+
+# private
+  def dinosaur_params
+    params.require(:dinosaur).permit(:name, :age, :image_url)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   root 'dinosaurs#index'
 
+
+# ORDER OF THE ROUTES MATTERS: WHY????
   get '/dinosaurs', to: 'dinosaurs#index'
   # get "dinosaurs" => "dinosaurs#index" ////Alternate
+  get "dinosaurs/new" => "dinosaurs#new", as: :new_dinosaur
+  get "dinosaurs/:id" => "dinosaurs#show", as: :dinosaur
 
+  post "dinosaurs" => "dinosaurs#create"
 end
